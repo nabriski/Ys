b33706f661cefe01085fe7b011fabd58f27c8509
Itamar Nabriski <itamar.nabriski@gmail.com>
2012-08-29 14:23:34 +0300

    more unit tests

--- tests/test_responses.js
+++ tests/test_responses.js
@@ -7,6 +7,15 @@ module.exports = {
         Ys("^/$").get = function(req,res){
             res.end("Hello World!");
         }
+        
+        Ys("^/raw_html/$").get = function(req,res){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end("<h1>Hello World!</h1>");
+        }
+
+        Ys("^/json/$").get.json = function(req,res){
+            res.returnObject({"message" : "Hello World"});
+        } 
 
         /*Ys("^/raw_html/$").get = function(req,res){
             res.writeHead(200, {'Content-Type': 'text/html'});
@@ -27,14 +36,45 @@ module.exports = {
     },
     test_get: function (test) {
 
-        var request = require('request');
         request('http://localhost:8780/', function (error, res, body) {
-            
             test.equals(res.statusCode,200);
             test.equals(body,"Hello World!");
             test.done();
+         })
+         
+    },
 
+    test_raw_html: function (test) {
+
+        request('http://localhost:8780/raw_html/', function (error, res, body) {
+            test.equals(res.statusCode,200);
+            test.equals(res.headers['content-type'],"text/html");
+            test.equals(body,"<h1>Hello World!</h1>");
+            test.done();
+         })
+         
+    },
+
+    test_json: function (test) {
+
+        request('http://localhost:8780/json/', function (error, res, body) {
+            test.equals(res.statusCode,200);
+            test.equals(res.headers['content-type'],"application/json");
+            test.equals(JSON.parse(body)["message"],"Hello World");
+            test.done();
          })
          
     }
+
+    /*
+     
+    Ys("^/raw_html/$").get = function(req,res){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end("<h1>Hello World!</h1>");
+}
+
+Ys("^/json/$").get.json = function(req,res){
+    res.returnObject({"message" : "Hello World"});
+} 
+     */
 };
